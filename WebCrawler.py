1c8e584e3fa8bf1ee65f5012d1298ee389eae433
charnugagoo <charnugagoo.gmail.com>
2013-02-05 11:20:35 -0500

    HashTable
    zhuoran

--- WebCrawler.py
+++ WebCrawler.py
@@ -10,6 +10,10 @@ class Parser(htmllib.HTMLParser):
 
 #argv = sys.argv
 argv = [1, 1, 11]
+#zhuoran begin
+number_visited_url = 0 #number of visited urls
+hash_table = {} #hash table url:hash_number
+#zhuoran end
 if len(argv) < 3:
     sys.exit("Please give a query (a set of keywords) and a number n!")
 queue = collections.deque([])
@@ -18,10 +22,14 @@ urlKey = "unescapedUrl"
 results0_7 = urllib2.urlopen(url + "&rsz=8")
 for result in json.load(results0_7)["responseData"]["results"]:
     queue.append(result[urlKey])
+    hash_table[result[urlKey]] = number_visited_url #zhuoran
+    number_visited_url += 1 #zhuoran
 results0_7.close()
 results8_9 = urllib2.urlopen(url + "&rsz=2&start=8")
 for result in json.load(results8_9)["responseData"]["results"]:
     queue.append(result[urlKey])
+    hash_table[result[urlKey]] = number_visited_url #zhuoran
+    number_visited_url += 1 #zhuoran
 results8_9.close()
 pagesNumber = int(argv[2])
 pagesCollected = 0
@@ -33,3 +41,7 @@ while len(queue) > 0 and pagesCollected < pagesNumber:
     parser.feed(open(linkFileName).read())
     parser.close()
     pagesCollected += 1
+    break
+    
+
+
