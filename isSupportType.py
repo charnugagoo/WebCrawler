2617df929a908e6f9c7527ce5d3bda02f231dd52
JiankaiDang <jiankaidang@gmail.com>
2013-02-15 11:15:31 -0500

    isSupportType

--- isSupportType.py
+++ isSupportType.py
@@ -1,25 +1,28 @@
 #import DeleteLastSlash
 
 #if not supported, return -1
-def isSupportType (href):
-    not_support_list = {"7z":0, "flv":1, "cgi":2, "xml":3, "avi":4, "ram":5, "mpg":6, "mov":7, "qt":8, "mp3":9, "mp4":10, "aiff":11, "au":12, "wav":13, "ra":14, "zip":15, "sit":16, "tar":17, "pdf":18}
-    support_list = {"html":0, "txt":1, "ssi":2, "shtml":3, "pl":4, "cfm":5, "asp":6, "lasso":7, "xml":8}
-    
+def isSupportType(href):
+    return href
+    not_support_list = {"7z": 0, "flv": 1, "cgi": 2, "xml": 3, "avi": 4, "ram": 5, "mpg": 6, "mov": 7, "qt": 8,
+                        "mp3": 9, "mp4": 10, "aiff": 11, "au": 12, "wav": 13, "ra": 14, "zip": 15, "sit": 16, "tar": 17,
+                        "pdf": 18}
+    support_list = {"html": 0, "txt": 1, "ssi": 2, "shtml": 3, "pl": 4, "cfm": 5, "asp": 6, "lasso": 7, "xml": 8}
+
     #http://www.baidu.com/wowo.wowo.avi
     #http://www.baidu.com
-#    href = DeleteLastSlash.DeleteLastSlash(href)
-#    added in check url function
+    #    href = DeleteLastSlash.DeleteLastSlash(href)
+    #    added in check url function
     strlist = href.split('/')
-    if len(strlist) >= 4 :
-        str = strlist[len(strlist)-1]
+    if len(strlist) >= 4:
+        str = strlist[len(strlist) - 1]
         strlist = str.split('.')
-        if len(strlist) >= 2 :
-            str = strlist[len(strlist)-1]
+        if len(strlist) >= 2:
+            str = strlist[len(strlist) - 1]
             if not support_list.has_key(str):
                 return -1
     return href
 
-#print isSupportType("http://www.baidu.com/wowo.wowo.avi")
-#print isSupportType("http://www.baidu.com")
-#print isSupportType("http://www.google.com/+1/button")
-#isSupportType("http://googleblog.blogspot.com/2011/03/1s-right-recommendations-right-when-you.html")
+    #print isSupportType("http://www.baidu.com/wowo.wowo.avi")
+    #print isSupportType("http://www.baidu.com")
+    #print isSupportType("http://www.google.com/+1/button")
+    #isSupportType("http://googleblog.blogspot.com/2011/03/1s-right-recommendations-right-when-you.html")
